94393ccbff896be870478c2ccf78d94ecc9c8ffd
sugianto228 <33213090+sugianto228@users.noreply.github.com>
2017-12-12 00:09:34 +0800

    Add files via upload

--- static/js/login.js
+++ static/js/login.js
@@ -1,9 +1,18 @@
-function checkForms(){
-    if($("#passwordNo1").val() == $("#passwordNo2").val()){
-        alert("Register successful!");
-        return true;
+function CHECK(){
+    if ($("#passwordNo1").val() != $("#passwordNo2").val()){
+        $("#registerErrorMessage").css("display","block");
     }else{
-        alert("Make sure both password are the same!");
+        $("#registerErrorMessage").css("display","none");
+    }
+}
+
+function CHECK2(){
+    if ($("#passwordNo1").val() != $("#passwordNo2").val()){
+        $("#registerErrorMessage").css("display","block");
         return false;
+    }else{
+        $("#registerErrorMessage").css("display","none");
+        alert("Register successfully!");
+        return true;
     }
 }
