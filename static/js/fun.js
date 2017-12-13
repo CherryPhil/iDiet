24c57ecd1dc692e33bb5a05c076a2e6b42de1d44
sugianto228 <33213090+sugianto228@users.noreply.github.com>
2017-12-13 19:29:33 +0800

    Add files via upload

--- static/js/fun.js
+++ static/js/fun.js
@@ -2,7 +2,7 @@ var question = [["Maximum carbohydrates are obtained from:", "A"],
                 ["Vitamins, minerals and proteins in suitable amounts are given to body by:", "D"],
                 ["According to food guide pyramid fats oils and sweets should be used:", "C"],
                 ["Sources of proteins includes:", "D"],
-                ["A diet containing right amount of energy, carbohydrates, proteins, fats, fiber, vitamins, minerals and water to fulfill requirement of body is called:", "B"],
+                ["A diet containing right amount of energy, carbohydrates, proteins, fats, fiber, vitamins, minerals and water to fulfill requirement of body is called:", "B"]
                 ];
 var question_choice = [["Whole grain food", "Fatty fish", "Plant oil", "Nuts"],
                         ["Whole grain food", "Fatty fish", "Plant oil", "Nuts"],
@@ -10,17 +10,62 @@ var question_choice = [["Whole grain food", "Fatty fish", "Plant oil", "Nuts"],
                         ["Fish", "Poultry", "Eggs", "All of them"],
                         ["Nutrition", "Balanced diet", "Perfect diet", "Food pyramid"]
                         ];
+var question2 = [["The following are good sources of protein:", "D"],
+                ["Why do you need vitamin D:", "C"],
+                ["Which is the best carbohydrate choice:", "B"],
+                ["Fat is essential to a healthy diet, but not all fats are created equal. Which of the following is a monounsaturated fat, considered to be the healthiest kind of fat for cooking:", "A"],
+                ["A protein is called a complete protein when it:", "D"]
+                ];
+var question_choice2 = [["Milk", "Beans", "Lean beef", "All of the above"],
+                        ["To help with digestion", "To help your hair grow", "To help calcium build bones", "So vitamin C won't get lonely"],
+                        ["White bread", "Whole-wheat bread", "White rice", "Fruit juice"],
+                        ["Olive oil", "Butter", "Coconut oil", "Lard"],
+                        ["Is low in fat and high in fiber", "Can be used in place of an egg in a recipe", "Includes white and dark meat", "Contains all essential amino acids, which the body cannot produce on its own"]
+                        ];
+var question3 = [["What are the Essential Nutrients:", "D"],
+                ["Which is NOT one of Galen's 'Laws of Health':", "B"],
+                ["What is metabolism determined by:", "D"],
+                ["What are Dietary Standards based on:", "D"],
+                ["What are the first three things to look for on a food label:", "C"]
+                ];
+var question_choice3 = [["Carbohydrates, Lipids (fats), and Proteins", "Vitamins and Minerals", "A & B and Water", "A & B, Water and Phytochemicals"],
+                        ["Eat the right foods", "Drink 8 glasses of water daily", "Breathe fresh air", "Get enough sleep"],
+                        ["Sleep", "Genetics", "Thyroid Gland", "All of the Above"],
+                        ["Large groups of people", "Individual diets", "B & C only", "All of the above"],
+                        ["1: Fat content, 2: Calorie content, 3: Serving size", "1: Serving size, 2: Servings per container, 3: Carbohydrate content", "1: Fat content, 2: Servings size, 3: Servings per container", "1: Serving size, 2: Fat content, 3: Servings per container"]
+                        ];
+
+var questions = question;
+var question_choices = question_choice;
+
+$("#challengeButton").click(function(){
+    questions = question;
+    question_choices = question_choice;
+    restartFunc();
+});
+$("#challengeButton2").click(function(){
+    questions = question2;
+    question_choices = question_choice2;
+    restartFunc();
+});
+$("#challengeButton3").click(function(){
+    questions = question3;
+    question_choices = question_choice3;
+    restartFunc();
+});
+
+
 var count = 0;
 var score = 0;
-$("#challengeButton").click(function(){
+function restartFunc(){
     score = 0;
     count = 0;
     $("#questionNumber").text(count + 1);
-    $("#modalBodyQuestion").text(question[count][0]);
-    $("#C1").text(question_choice[count][0]);
-    $("#C2").text(question_choice[count][1]);
-    $("#C3").text(question_choice[count][2]);
-    $("#C4").text(question_choice[count][3]);
+    $("#modalBodyQuestion").text(questions[count][0]);
+    $("#C1").text(question_choices[count][0]);
+    $("#C2").text(question_choices[count][1]);
+    $("#C3").text(question_choices[count][2]);
+    $("#C4").text(question_choices[count][3]);
     $("#modalProgress").css("width", "0%");
     $("#modalProgressNo").text(0);
     $("#modalProgress").attr("aria-valuenow", "0");
@@ -28,31 +73,41 @@ $("#challengeButton").click(function(){
     $("#B").prop("checked", false);
     $("#C").prop("checked", false);
     $("#D").prop("checked", false);
-});
+    $("#errorMessage").css("display", "none");
+    $("#modalBodyNext").text("Next Question");
+};
 
-$("#modalBodyNext").click(function(){
-    if($("#"+question[count][1]).prop("checked")){
+function hi(){
+    if($("#A").prop("checked") == false && $("#B").prop("checked") == false && $("#C").prop("checked") == false && $("#D").prop("checked") == false){
+        $("#errorMessage").css("display", "block");
+    }else{
+        if ($("#" + questions[count][1]).prop("checked")) {
         score = score + 1;
     }
+    $("#errorMessage").css("display", "none");
     $("#A").prop("checked", false);
     $("#B").prop("checked", false);
     $("#C").prop("checked", false);
     $("#D").prop("checked", false);
-    var progress_no = Number($("#modalProgressNo").text()) +20;
-    $("#modalProgress").css("width", progress_no+"%");
-    $("#modalProgressNo").text( progress_no );
-    $("#modalProgress").attr("aria-valuenow", progress_no );
+    var progress_no = Number($("#modalProgressNo").text()) + 25;
+    $("#modalProgress").css("width", progress_no + "%");
+    $("#modalProgressNo").text(progress_no);
+    $("#modalProgress").attr("aria-valuenow", progress_no);
 
-    if(count == 4){
-        alert("Congratulation! You have done the challenge, you got "+score+" question correct!!");
+    if (count == 3) {
+        $("#modalBodyNext").text("Finish");
+    }
+    if (count == 4) {
+        alert("Congratulation! You have done the challenge, you got " + score + " question correct!!");
         $('#challengeModal').modal("toggle");
     }
 
     count = count + 1;
     $("#questionNumber").text(count + 1);
-    $("#modalBodyQuestion").text(question[count][0]);
-    $("#C1").text(question_choice[count][0]);
-    $("#C2").text(question_choice[count][1]);
-    $("#C3").text(question_choice[count][2]);
-    $("#C4").text(question_choice[count][3]);
-});
+    $("#modalBodyQuestion").text(questions[count][0]);
+    $("#C1").text(question_choices[count][0]);
+    $("#C2").text(question_choices[count][1]);
+    $("#C3").text(question_choices[count][2]);
+    $("#C4").text(question_choices[count][3]);
+    }
+}
