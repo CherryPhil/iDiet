40902087b1ffaabac58d79f627b012e74dc8bafb
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2017-12-14 01:32:06 +0800

    Add files via upload

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,48 +1,40 @@
 window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title: {
+			text: "Your BMI Progress"
+		},
+        axisX:{
+		    title:"Months"
 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	theme: "light2",
-	title:{
-		text: "Your BMI Progress"
-	},
-    axisX:{
-	    title:"Months"
-    },
-	axisY:{
-	    title: "B M I",
-		includeZero: false
-	},
-	data: [{
-		type: "line",
-		dataPoints: [
-			{x:1, y: 26 },
-			{x:2, y: 26},
-			{x:3, y: 25 },
-			{x:4, y: 25 },
-			{x:5, y: 23 },
-			{x:6, y: 24 },
-			{x:7, y: 26 , indexLabel: "highest",markerColor: "red", markerType: "triangle"},
-			{x:8, y: 24 },
-			{x:9, y: 23  },
-			{x:10, y: 21 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross"}
+        },
+        axisY: {
+            title: "B M I",
+            includeZero: false
+        },
+		data: [
+		{
+			type: "line",
+			dataPoints: [
+				{ y: 10 },
+				{ y:  4 },
+				{ y: 18 },
+				{ y:  8 }
+			]
+		}
 		]
-	}]
+	});
+	chart.render();
 
-});
-chart.render();
+	$("#addDataPoint").click(function () {
+
+	var length = chart.options.data[0].dataPoints.length;
+	chart.options.title.text = "Your BMI Progress";
+	chart.options.data[0].dataPoints.push({y:parseInt(document.getElementById("newdp").value)});
+	chart.render();
+
+
+	});
 
 };
 
-function addDataPointsAndRender() {
-    xValue = Number(document.getElementById('xValue').value);
-    yValue = Number(document.getElementById('output').value);
-    dps.push({
-        x: xValue,
-        y: yValue
-    });
-    chart.render();
-}
 
-var renderButton = document.getElementById('renderButton');
-renderButton.addEventListener('click', addDPsAndRender);
