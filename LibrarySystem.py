3b1d32082dd1a467cb3b6b482a416a6f7b3e922b
CherryPhil <32829480+CherryPhil@users.noreply.github.com>
2017-12-15 01:05:51 +0800

    Add files via upload

--- LibrarySystem.py
+++ LibrarySystem.py
@@ -4,7 +4,12 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 from postVar import Post
+from postVar import Contact
+from postVar import User_recipe
+
 from getPost import postObj
+from getPost import contactObj
+from getPost import recipeObj
 
 cred = credentials.Certificate('cred/idiet-229a2-firebase-adminsdk-f5ibn-9f138ec335.json')
 default_app = firebase_admin.initialize_app(cred, {
@@ -47,9 +52,23 @@ def general():
 def recipes():
     return render_template("recipes.html")
 
-@app.route('/community/contactus')
+@app.route('/community/contactus', methods=['POST', 'GET'])
 def contactus():
-    return render_template("contactus.html")
+    contact = Contact(request.form)
+    if request.method == 'POST':
+        email = contact.email.data
+        subject = contact.subject.data
+        message = contact.message.data
+        contacts = contactObj(email, subject, message)
+        contact_db = root.child('messages')
+        contact_db.push({
+            'email': contacts.get_email(),
+            'subject': contacts.get_subject(),
+            'message': contacts.get_message(),
+        })
+        return redirect(url_for('contactus'))
+
+    return render_template("contactus.html", contact=contact)
 
 @app.route('/community/faq')
 def faq():
@@ -76,11 +95,28 @@ def post():
         post_db.push({
             'title': posts.get_title(),
             'comment': posts.get_comment(),
-
         })
         return redirect(url_for('general'))
 
     return render_template("post.html", post=post)
+
+@app.route('/community/recipes/post_recipe', methods=['POST', 'GET'])
+def post_recipe():
+    postR = User_recipe(request.form)
+    if request.method == 'POST':
+        food = postR.food.data
+        food_type = postR.food_type.data
+        recipes = postR.recipes.data
+        postRs = recipeObj(food, food_type, recipes)
+        postR_db = root.child('recipes')
+        postR_db.push({
+            'food': postRs.get_food(),
+            'food_type': postRs.get_food_type,
+            'recipes': postRs.get_recipes,
+        })
+        return redirect(url_for('recipes'))
+
+    return render_template("post_recipe.html", postR=postR)
 
 if __name__ == '__main__':
     app.secret_key = 'iDiet123'
